b7360fa8d8356d3c6ce40d23e9be6bcdb50fdee0
Manjurul Haque <manjurulk@frontiersemi.com>
2024-06-25 21:50:23 +0600

    numeric minimum and maximum limits

---
+++ MinMax.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <numeric>
+
+using namespace std;
+
+int main()
+{
+	float fMin = numeric_limits<float>::min();	// most minimum number in float
+	float fMax = numeric_limits<float>::max();	// most maximum number in float
+
+	int iMin = numeric_limits<int>::min();	// most minimum number in int
+	int iMax = numeric_limits<int>::max();	// most maximum number in int
+	return 0;
+}
